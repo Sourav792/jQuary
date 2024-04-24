faf431a32bca2e521c2606ddc2a0b6823edde8a7
Sourav Singh <sourav859987@gmail.com>
2024-04-24 10:19:08 +0530

    delete file

--- jquaryEffects/chaining.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  // $("p").css("color", "red").slideUp(2000).slideDown(2000);
-
-  $("p")
-  .css("color", "red")
-  .slideUp(2000)
-  .slideDown(2000);
-});
--- jquaryEffects/sliding.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    // $(selector).slideDown(speed,callback);
-
-    $("button").click(function(){
-        // $(".panel").slideDown();
-        // $(".panel").slideUp();
-        $(".panel").slideToggle();
-      });
-})
