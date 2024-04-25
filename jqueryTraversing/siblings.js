db8320425926330ad0a3acfa0561b7fb7217815e
Sourav Singh <sourav859987@gmail.com>
2024-04-25 15:34:43 +0530

    learn traversing siblings using jquery

---
+++ jqueryTraversing/siblings.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // $("h2").siblings().css({"color": "aqua", "border": "2px solid red"});
+
+    // $("h2").next().css({"color": "aqua", "border": "2px solid red"});
+
+    // $("h2").next().css({"color": "red", "border": "2px solid red"});
+
+    // $("h2").nextAll().css({"color": "red", "border": "2px solid red"});
+
+    // $("h2").nextUntil('h6').css({"color": "red", "border": "2px solid red"});
+
+    // $("h2").prev().css({"color": "aqua", "border": "2px solid red"});
+
+    // $("h2").prevAll().css({"color": "aqua", "border": "2px solid red"});
+
+   
+
+  });
