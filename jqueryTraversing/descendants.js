b6255eadf0786f7fe2ef3825bb1121be4d0692b0
Sourav Singh <sourav859987@gmail.com>
2024-04-25 15:22:50 +0530

    learn traversing descendants element using jquery

---
+++ jqueryTraversing/descendants.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // $("div").children().css({"color": "red", "border": "2px solid aqua"});
+
+    // $("div").children("p:odd").css({"color": "red", "border": "2px solid red"});
+
+    // $("div").find("span").css({"color": "red", "border": "5px solid yellow"});
+
+    $("div").find("*").css({"color": "red", "border": " 2px solid blue"}); 
+  });
