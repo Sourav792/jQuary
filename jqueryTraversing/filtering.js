d7d1ae247c73cd0e8337bdff525daf856a824b83
Sourav Singh <sourav859987@gmail.com>
2024-04-25 15:45:39 +0530

    learn filtering using jquery

---
+++ jqueryTraversing/filtering.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // $("div").first().css("background-color", "yellow");
+
+    // $("div").last().css("background-color", "yellow");
+
+    // $("p").eq(0).css("background-color", "yellow");
+
+    // $("p").filter('.intro').css("background-color", "yellow");
+
+    $("p").not('.intro').css("background-color", "yellow");
+    
+
+})
